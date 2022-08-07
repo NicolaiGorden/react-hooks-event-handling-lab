714cf77c9d3ea2bf4f41867072058016545074bd
Nicolai <Nicolaigorden@gmail.com>
2022-08-07 12:28:35 -0400

    commit tree 486be0950c2841931a39aa11b06dc1a2395e4d5b

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+    return (
+        <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    )
+}
+
+function focus(event){
+    console.log('Good!')
+}
+
+function blur(event){
+    console.log('Hey! Eyes on me!')
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
-// Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input onChange = {handleChange} type="password"/>
+        </div>
     )
+}
+
+function handleChange(event){
+    console.log('Entering password...')
 }
 
 export default Keypad;
